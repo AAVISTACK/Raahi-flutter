31b05775a4999d81d933784b86ce3ac2313175fc
AAVISTACK <63523107+AAVISTACK@users.noreply.github.com>
2026-03-16 04:50:48 +0530

    config: update to new Firebase project raahi-6f418

---
+++ raahi/lib/firebase_options.dart
@@ -0,0 +1,48 @@
+// File generated/updated for Firebase project raahi-6f418
+// ignore_for_file: type=lint
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web. '
+        'You can reconfigure this by running the FlutterFire CLI again.',
+      );
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for iOS.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macOS.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for Windows.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for Linux.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey:            'FIREBASE_KEY',
+    appId:             '1:107641072718:android:ff4d70f55d55f0e055a71f',
+    messagingSenderId: '107641072718',
+    projectId:         'raahi-6f418',
+    storageBucket:     'raahi-6f418.firebasestorage.app',
+  );
+}
